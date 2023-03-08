1828f4757a8a9e09b1118a4e434c724ceab0825f
Blaq-Catz <74494823+Blaq-Catz@users.noreply.github.com>
2023-03-08 01:33:09 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,1 +1,5 @@
-dsfgsjfsdgfjhsg
+#!/usr/bin/python3
+for i in range(97, 123):
+    tableAsciiToAlphabet = chr(i)
+    asciiToAlpha = "" + tableAsciiToAlphabet
+    print("{}".format(asciiToAlpha), end='')
