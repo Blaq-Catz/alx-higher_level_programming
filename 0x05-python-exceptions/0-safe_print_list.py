06d3da7b13f50d52145e52d50727c59b4c999b60
Blaq-Catz <74494823+Blaq-Catz@users.noreply.github.com>
2023-03-28 22:29:51 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,11 @@
-asdjhasdjhaj
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+        except IndexError:
+            print()
+            return i
+
+    print()
+    return x
